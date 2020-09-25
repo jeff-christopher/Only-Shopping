a0a10bedd818acf4355b43a4b6f12918236932d8
jeff-christopher <jeffchristopher998@gmail.com>
2020-09-25 01:42:43 -0600

    User model was created, a dummy user was added for testing purposes, and all products retrived from db are based on the user logged in.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 /**
+ * Temporal
+ */
+const User = require('./models/user');
+
+/**
  * Core node packages 
  */
 const path = require('path');
@@ -28,19 +33,56 @@ const errorController = require('./controllers/error');
  */
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Middlewares
+ */
+app.use(async(req, res, next) => {
+    req.user = await User.findByPk(1);
+    next();
+});
+
+/**
+ * RoutesMiddlewares
+ */
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404View);
 
 /**
+ * Define relations between models
+ */
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+/**
  * Sequilize sync
+ * Create all models in case it's needed.
  */
 
 sequelize.sync({ force: false })
+    .then(
+        response => {
+            return User.findByPk(1);
+        }
+    )
+    .then(
+        user => {
+            if (!user) {
+                return User.create({
+                    name: 'Jeff',
+                    email: 'jeff.ortiz@dev.test',
+                });
+            } else {
+                return user;
+            }
+        }
+    )
     .then(
         response => {
             app.listen(3000);
